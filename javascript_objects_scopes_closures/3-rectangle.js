968daa308420c7e6aaa000c0c746af6d0a9e4b3e
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-09-07 12:56:20 +0300

    initial commit

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    print() {
+        for(){
+            return (X*w)
+        }
+
+    }
+}
+
+module.exports= Rectangle
---
+++ javascript_objects_scopes_closures/README.md
@@ -0,0 +1,6 @@
+Everything in JavaScript is an object except primitives.
+Object is a collection of Js related data or functionality.
+Here we take a deep dive into:
+   1. objects
+   2. scopes
+   3. closures
