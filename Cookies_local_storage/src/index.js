3350e46ac787f58114bc1717b9e34a5e2e08401d
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-09-21 15:51:14 +0300

    set expiry

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -3,9 +3,12 @@ function setCookies() {
     const email = document.getElementById('email');
     const nameValue = firstname.value;
     const emailValue = email.value;
+    // expiration
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 10);
 
-    document.cookie = `firstname=${nameValue}`;
-    document.cookie = `email=${emailValue}`;
+    document.cookie = `firstname=${nameValue}; expires=${expirationDate.toUTCString()}`;
+    document.cookie = `email=${emailValue}; expires=${expirationDate.toUTCString()}`;
 
     alert("Cookies set Successfully")
    
