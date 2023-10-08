9f1a27bf240a449341c6e95f4e23e1660fab373a
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-08 22:42:47 +0300

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = "https://swapi-api.alx-tools.com/api/films/";
+const url = process.argv[2];
 
 request.get(url, function(error, response, body) {
   if (error) {
@@ -15,7 +15,7 @@ request.get(url, function(error, response, body) {
   // script that prints the number of movies where the character “Wedge Antilles” is present
   const films= JSON.parse(body).results;
   const characterId = "18"; // Wedge Antilles character ID
-  const wedgeAntillesFilms = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+  const wedgeAntillesFilms = films.filter(film => film.characters.includes(`${url}+/+${characterId}/`));
 
   console.log(wedgeAntillesFilms.length)
 
