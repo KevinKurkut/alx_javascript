635d502fc8159b8f93b0fe4da76c4f7dd7341042
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-08 23:13:55 +0300

    2-starwars_count.js

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = process.argv[2];
+const url = "https://swapi-api.alx-tools.com/api/films/";
 
 request.get(url, function(error, response, body) {
   if (error) {
