846cf0b5592c57fb88bf3cb1af3204d1f50e68d5
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-09 14:19:42 +0300

    Loripsum

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const fs = require('fs')
 request(process.argv[2], function(error, response, body) {
-    if (err == null){
+    if (error == null){
         fs.writeFileSync(process.argv[3].body)
     }
 });
